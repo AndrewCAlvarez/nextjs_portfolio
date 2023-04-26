61e76c385ba0d13a3336fcacde3d2da4dd5c2590
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2023-04-26 14:19:43 -0400

    Add styling to hero section.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,18 +4,18 @@ import ParticlesDemo from "./ParticlesDemo";
 
 export default function Hero() {
   return (
-    <section className={utils.container}>
+    <section className={`${styles.hero} ${utils.container}`}>
       <ParticlesDemo />
 
       <p className={styles.tag}>Hi, my name is</p>
-      <h1>Andrew Alvarez</h1>
+      <h1 className={styles.name}>Andrew Alvarez</h1>
       <h1 className={styles.subHeading}>Let's build the web.</h1>
       <p className={styles.text}>
         My approach to web development is to combine creativity with
         functionality. I believe that a website should not only look great but
         also be easy to use and navigate.{" "}
       </p>
-      <button>Let's build something!</button>
+      <button className={styles.button}>Let's build something!</button>
     </section>
   );
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,9 +5,16 @@ import utils from "../styles/utils.module.css";
 
 export default function Nav() {
   return (
-    <nav className={(styles.nav, utils.container)}>
+    <nav className={`${styles.nav} ${utils.container}`}>
       <div>A</div>
-      {/* <Image className="icon-menu" priority src={menuIcon} alt="Menu icon." /> */}
+      <Image
+        className={styles.iconMenu}
+        priority
+        src={menuIcon}
+        alt="Menu icon."
+        width={40}
+        height={40}
+      />
     </nav>
   );
 }
