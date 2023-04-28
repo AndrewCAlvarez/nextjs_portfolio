fe6e6c1d83b8873771cbe1784eacf05ececbd5cc
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2023-04-28 11:46:30 -0400

    Implement stack and display images in about me section.

--- components/About.tsx
+++ components/About.tsx
@@ -1,29 +1,86 @@
 import Image from "next/image";
 import utils from "../styles/utils.module.css";
+import styles from "./About.module.css";
 
 export default function About() {
+  function handleDisplayPic() {
+    console.log("enter");
+    let pics = document.querySelectorAll(`.${styles.profilePic}`);
+    let index = 0;
+    pics.forEach((pic) => {
+      pic.className = [
+        styles.profilePic,
+        styles[`profilePic${index}Display`],
+      ].join(" ");
+      index++;
+    });
+    console.log(pics);
+  }
+
+  function handleHidePics() {
+    console.log("Left");
+    let pics = document.querySelectorAll(`.${styles.profilePic}`);
+    let index = 0;
+    pics.forEach((pic) => {
+      pic.className = [styles.profilePic, styles[`profilePic${index}`]].join(
+        " "
+      );
+      index++;
+    });
+  }
+
   return (
-    <section className={utils.container}>
-      <h2>About Me</h2>
-      <p>
-        - I'm Andrew Alvarez, a web developer with a passion for creating
-        beautiful and functional websites. With over [insert number] years of
-        experience in the industry, I have honed my skills in web development
-        and design to deliver exceptional results to my clients. - My expertise
-        lies in [insert areas of specialization], and I am always up for a new
-        challenge. Whether you need a simple website or a complex web
+    <section className={`${styles.about} ${utils.container}`}>
+      <h2 className={styles.heading}>About Me</h2>
+      <div
+        className={styles.picContainer}
+        onMouseOver={handleDisplayPic}
+        onMouseLeave={handleHidePics}
+      >
+        <Image
+          className={[styles.profilePic, styles.profilePic0].join(" ")}
+          src="/../public/cowboy_profile_pic.jpg"
+          alt="Picture of Andrew."
+          width={200}
+          height={200}
+        />
+        <Image
+          className={[styles.profilePic, styles.profilePic1].join(" ")}
+          src="/../public/cowboy_profile_pic.jpg"
+          alt="Picture of Andrew."
+          width={200}
+          height={200}
+        />
+        <Image
+          className={[styles.profilePic, styles.profilePic2].join(" ")}
+          src="/../public/cowboy_profile_pic.jpg"
+          alt="Picture of Andrew."
+          width={200}
+          height={200}
+        />
+        <Image
+          className={[styles.profilePic, styles.profilePic3].join(" ")}
+          src="/../public/cowboy_profile_pic.jpg"
+          alt="Picture of Andrew."
+          width={200}
+          height={200}
+        />
+      </div>
+      <p className={styles.text}>
+        I'm Andrew Alvarez, a web developer with a passion for creating
+        beautiful and functional websites. I have honed my skills in web
+        development and design to deliver exceptional results. My expertise lies
+        in frontend development using frameworks like React, and I am always up
+        for a new challenge. Whether you need a simple website or a complex web
         application, I have the knowledge and skills to bring your vision to
-        life. - Take a look at my portfolio to see some of my recent projects,
-        and feel free to get in touch with me to discuss how I can help you
-        create a stunning website that represents your brand and engages your
-        audience.
+        life.
       </p>
-      <Image
-        src="/../public/cowboy_profile_pic.jpg"
-        alt="Picture of Andrew."
-        width={100}
-        height={100}
-      />
+
+      <p className={styles.text}>
+        Take a look at my portfolio to see some of my recent projects, and feel
+        free to get in touch with me to discuss how I can help you create a
+        stunning website that represents your brand and engages your audience.
+      </p>
     </section>
   );
 }
