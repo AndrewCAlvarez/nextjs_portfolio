71eef6c15daa13962508286569184954864abddf
Andrew C Alvarez <31179195+AndrewCAlvarez@users.noreply.github.com>
2023-05-01 19:02:05 -0400

    Made a lot of changes. About me section heading color and border. Green outline on headshot. Green border around first paragraph with psuedo element tag.

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import utils from "../styles/utils.module.css";
 import styles from "./About.module.css";
+import headshot from "../public/headshot.jpg";
+import { useEffect } from "react";
 
 export default function About() {
   function handleDisplayPic() {
@@ -18,61 +20,63 @@ export default function About() {
 
   function handleHidePics() {
     console.log("Left");
-    let pics = document.querySelectorAll(`.${styles.profilePic}`);
+    let pics: NodeListOf<Element> = document.querySelectorAll(
+      `.${styles.profilePic}`
+    );
     let index = 0;
     pics.forEach((pic) => {
-      pic.className = [styles.profilePic, styles[`profilePic${index}`]].join(
-        " "
-      );
+      // pic.className = [styles.profilePic, styles[`profilePic${index}`]].join(
+      //   " "
+      // );
+      // pic.classList.toggle(styles[`profilePic${index}`]);
       index++;
     });
   }
 
+  useEffect(() => {
+    console.log("Use Effect");
+  }, []);
+
   return (
-    <section className={`${styles.about} ${utils.container}`}>
-      <h2 className={styles.heading}>About Me</h2>
-      <div
-        className={styles.picContainer}
-        onMouseOver={handleDisplayPic}
-        onMouseLeave={handleHidePics}
-      >
-        {/* This is a test for the glitch effect */}
-        <Image
-          className={[styles.glitch, styles.glitch1].join(" ")}
-          src="/../public/cowboy_profile_pic.jpg"
-          alt="Picture of Andrew."
-          // width={250}
-          // height={250}
-          fill
-        />
-        <Image
-          className={[styles.glitch, styles.glitch2].join(" ")}
-          src="/../public/cowboy_profile_pic.jpg"
-          alt="Picture of Andrew."
-          // width={250}
-          // height={250}
-          fill
-        />
-        <Image
-          className={[styles.glitch, styles.glitch3].join(" ")}
-          src="/../public/cowboy_profile_pic.jpg"
-          alt="Picture of Andrew."
-          // width={250}
-          // height={250}
-          fill
-        />
+    <section className={styles.about}>
+      <h2 className={[styles.heading, styles["slide-top"]].join(" ")}>
+        About Me
+      </h2>
+      <div className={styles.picContainer}>
+        {/* This is for the glitch effect */}
+        {/* <Image
+        className={[styles.glitch, styles.glitch1].join(" ")}
+        src={headshot}
+        alt="Picture of Andrew."
+        // width={250}
+        // height={250}
+      />
+      <Image
+        className={[styles.glitch, styles.glitch2].join(" ")}
+        src={headshot}
+        alt="Picture of Andrew."
+        // width={250}
+        // height={250}
+      />
+      <Image
+        className={[styles.glitch, styles.glitch3].join(" ")}
+        src={headshot}
+        alt="Picture of Andrew."
+        // width={250}
+        // height={250}
+      /> */}
+
         {/* Image gallery */}
         <Image
           className={[styles.profilePic, styles.profilePic0].join(" ")}
-          src="/../public/cowboy_profile_pic.jpg"
+          src={headshot}
           alt="Picture of Andrew."
           // width={250}
           // height={250}
-          fill
         />
-        <Image
+        {/* <Image
           className={[styles.profilePic, styles.profilePic1].join(" ")}
-          src="/../public/cowboy_profile_pic.jpg"
+          src={headshot}
           alt="Picture of Andrew."
           // width={200}
           // height={200}
@@ -80,7 +84,7 @@ export default function About() {
         />
         <Image
           className={[styles.profilePic, styles.profilePic2].join(" ")}
-          src="/../public/cowboy_profile_pic.jpg"
+          src={headshot}
           alt="Picture of Andrew."
           // width={200}
           // height={200}
@@ -88,13 +92,13 @@ export default function About() {
         />
         <Image
           className={[styles.profilePic, styles.profilePic3].join(" ")}
-          src="/../public/cowboy_profile_pic.jpg"
+          src={headshot}
           alt="Picture of Andrew."
           width={200}
           height={200}
-        />
+        /> */}
       </div>
-      <p className={styles.text}>
+      <p className={styles.text1}>
         I'm Andrew Alvarez, a web developer with a passion for creating
         beautiful and functional websites. I have honed my skills in web
         development and design to deliver exceptional results. My expertise lies
@@ -104,10 +108,10 @@ export default function About() {
         life.
       </p>
 
-      <p className={styles.text}>
+      <p className={styles.text2}>
         Take a look at my portfolio to see some of my recent projects, and feel
         free to get in touch with me to discuss how I can help you create a
-        stunning website that represents your brand and engages your audience.
+        website that represents your brand and engages your audience.
       </p>
     </section>
   );
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,20 +1,35 @@
 import utils from "../styles/utils.module.css";
+import styles from "./Experience.module.css";
+import Image from "next/image";
+import UrbanUndergroundHero from "../public/websites/UrbanUnderGroundHero.png";
+import KonreadsHero from "../public/websites/KonreadsHero.jpg";
 
 export default function Experience() {
   return (
-    <section className={utils.container}>
+    <section className={[styles.container, styles.wrapper].join(" ")}>
       <h2>Experience</h2>
       <ul>
-        <div>
+        <header className={styles.sectionHeader}>
+          <Image
+            className={styles.backgroundImage}
+            src={UrbanUndergroundHero}
+            alt="Hero section of portfolio project Urban Underground."
+          />
           <h3>Urban Underground</h3>
-          <p>
-            Urban Underground - HTML, CSS, Javascript - Urban Underground is
-            personal project where I took the lessons learned about web design
-            to create a beautiful page for a fictitious fashion magazine.{" "}
-          </p>
-          <a href="https://andrewcalvarez.github.io/rap-friends/">Link</a>
-        </div>
+        </header>
+        <p>
+          Urban Underground - HTML, CSS, Javascript - Urban Underground is
+          personal project where I took the lessons learned about web design to
+          create a beautiful page for a fictitious fashion magazine.{" "}
+        </p>
+        <a href="https://andrewcalvarez.github.io/rap-friends/">Link</a>
+
         <div>
+          <Image
+            src={KonreadsHero}
+            alt="Hero section of portfolio project Konreads"
+            width={400}
+          />
           <h3>Konreads</h3>
           <p>
             Konreads - HTML, CSS, Javascript - Dark and elegant, Konreads is a
